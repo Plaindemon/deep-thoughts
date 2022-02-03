43ada056ed9127d7b71df6a37e416eb58621d237
Plain Demon <89330268+Plaindemon@users.noreply.github.com>
2022-02-02 23:52:56 -0700

    added client

--- server/server.js
+++ server/server.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const path = require('path');
+
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
 const { authMiddleware } = require('./utils/auth');
 
 // import our typeDefs and resolvers
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 const startServer = async () => {
@@ -35,15 +36,15 @@ startServer();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Serve up static assets
+// // Serve up static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
-}
+
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+}
 
 db.once('open', () => {
   app.listen(PORT, () => {
